38000f32ac14b1db530ab80f656195c19c97742b
jun2k5 <jun2k5@naver.com>
2024-11-01 16:23:03 +0900

    real start

--- python/bac1.py
+++ python/bac1.py
@@ -1,40 +1,15 @@
-"""
-군 복학 후 아무것도 기억안나기에 일단
-파이썬으로 백준 1번부터 풀어보도록 하겠다.
-나중에 C, C#, C++, JAVA, Node.js 까지
-시도해보겠다.
-
-환경
-    OS : Windows 11 (Pro) x86_64
-    Kernel: WIN32_NT 10.0.22631.4391 (23H2)
-    Shell: CMD 10.0.22621.4391
-    CUP : AMD Ryzen 7 7700 (16) @ 5.35 GHz
-    GPU : NVIDIA GeForce RTX 3060 (7.85 GiB) [Discrete]
-    Memory : 31.72 GiB
-    Swap : 2.00 GiB
-"""
+# 백준 1. Hello World!를 출력하시오. 
 
 if __name__ == "__main__":
     print("Hello World!")
-    
+
+
+# 주석처리 Cntl + /
+# Visual Code 글자 확대 - Cntl + '+' 
 
 """
-Git 사용법
- 깃으로 관리할 작업 폴더를 만들고
- 작업 폴더에서 Window PowerShell을 연다.
- 여는 방법은 Shift + 우클릭 => 여기에 PowerShell 창 열기
- 
- 명령어
-    git config --global user.email "이메일 명"
-    git config --global user.name "이름"
-        //현재 깃 사용자 등록
-
-    
-애디터를 열고 작업파일 열기
-Terminal에서 new Terminal 클릭
-명령어
-    git init
-    
-
+-Learning Point
+    파이썬에서는 print("") 함수로 출력한다.
 
 """
+
