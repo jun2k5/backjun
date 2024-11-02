1bd1184544682c0cb20a5fcc99af486ccb015593
jun2k5 <jun2k5@naver.com>
2024-11-02 17:04:43 +0900

    20241102second

---
+++ python/bac6.py
@@ -0,0 +1,30 @@
+# 10430문제
+# (A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+
+# (A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+
+# 세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 A, B, C가 순서대로 주어진다. (2 ≤ A, B, C ≤ 10000)
+
+# 출력
+# 첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+
+def solution():
+    A, B, C = map(int, input().split())
+    print((A+B)%C)
+    print(((A%C) + (B%C))%C)
+    print((A*B)%C)
+    print(((A%C) * (B%C))%C)
+    
+
+
+if __name__ == "__main__":
+    solution()
+
+"""
+-Learning Point
+    print()함수 안에서 연산이 가능하다.
+
+"""
