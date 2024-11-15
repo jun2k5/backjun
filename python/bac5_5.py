8cf1c85a43cb382744ae8c3d54a31415ae135751
jun2k5 <jun2k5@naver.com>
2024-11-15 22:41:36 +0900

    20241115first

---
+++ python/bac5_5.py
@@ -0,0 +1,44 @@
+# 11720문제
+# N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 숫자의 개수 N (1 ≤ N ≤ 100)이 주어진다. 둘째 줄에 숫자 N개가 공백없이 주어진다.
+
+# 출력
+# 입력으로 주어진 숫자 N개의 합을 출력한다.
+
+def solution(N:int):
+    result = 0
+    
+    S = input()
+
+    for i in range(0, N):
+        try:
+            result += int(S[i])
+
+        except:
+            print("오류")
+            break
+
+    print(result)
+
+
+
+if __name__ == "__main__":
+
+    while True:
+        try:
+            N = int(input())
+            break
+        except:
+            print("오류")
+            print("다시입력")
+
+    solution(N)
+
+
+
+# ToDo : 5_4 5_5 주석달기
+
+
+
